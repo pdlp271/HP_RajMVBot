2fdfd46c8f84518ab3d42a5aa4e9c86e8c3f59eb
pdlp271 <109448588+pdlp271@users.noreply.github.com>
2025-07-03 20:07:59 +0530

    Update info.py

--- info.py
+++ info.py
@@ -61,9 +61,9 @@ OWNER_UPI_ID = environ.get('OWNER_UPI_ID', 'Not Available')
 # ============================
 # MongoDB Configuration
 # ============================
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://Hanuman:hanuman@cluster0.l5toshc.mongodb.net/?retryWrites=true&w=majority")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://HPRaj271:HPRaj271@cluster0.xoqwdlz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 DATABASE_URI2 = environ.get('DATABASE_URI2', "")
-DATABASE_NAME = environ.get('DATABASE_NAME', "Hanuman")
+DATABASE_NAME = environ.get('DATABASE_NAME', "HPRaj271")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'HPfiles')
 
 # ============================
