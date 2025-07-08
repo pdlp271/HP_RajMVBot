efc318a69d38552b07e8bba3433f5e27403264a5
pdlp271 <109448588+pdlp271@users.noreply.github.com>
2025-07-08 21:43:04 +0530

    Update pmfilter.py

--- plugins/pmfilter.py
+++ plugins/pmfilter.py
@@ -2124,7 +2124,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
     elif query.data == "about":
         buttons = [[
             InlineKeyboardButton('⚡ ᴍᴏᴠɪᴇꜱ ɢʀᴏᴜᴩ ⚡', url=f'https://t.me/+DWu_QXl2Ydg3N2Zl'),
-            InlineKeyboardButton ('〽️ ꜱᴜᴩᴩᴏʀᴛ ɢʀᴏᴜᴩ 〽️', url=f'https://t.me/+DWu_QXl2Ydg3N2Zl'),
+            InlineKeyboardButton ('〽️ ꜱᴜᴩᴩᴏʀᴛ ɢʀᴏᴜᴩ 〽️', url=f'https://t.me/+fzXLS_LbzSFjOTVl'),
         ],[
             InlineKeyboardButton('ᴅᴏɴᴀᴛɪᴏɴ 💰', callback_data='donation'), 
         ],[
@@ -2457,9 +2457,9 @@ async def auto_filter(client, msg, spoll=False):
                 cap += f"<b>\n<a href='https://telegram.me/{temp.U_NAME}?start=files_{file.file_id}'> 📁 {get_size(file.file_size)} ▷ {' '.join(filter(lambda x: not x.startswith('[') and not x.startswith('@') and not x.startswith('www.'), file.file_name.split()))}\n</a></b>"
     else:
         if settings["button"]:
-            cap = f"<b>🧿 ᴛɪᴛʟᴇ : <code>{search}</code>\n📂 ᴛᴏᴛᴀʟ ꜰɪʟᴇꜱ : <code>{total_results}</code>\n📝 ʀᴇǫᴜᴇsᴛᴇᴅ ʙʏ : {message.from_user.mention}\n⏰ ʀᴇsᴜʟᴛ ɪɴ : <code>{remaining_seconds} Sᴇᴄᴏɴᴅs</code>\n⚡ ᴩᴏᴡᴇʀᴇᴅ ʙʏ : @HP_Raj_Movies\n\n🌳 ʏᴏᴜʀ ʀᴇqᴜᴇꜱᴛᴇᴅ ꜰɪʟᴇꜱ 👇 \n</b>"
+            cap = f"<b>🧿 ᴛɪᴛʟᴇ : <code>{search}</code>\n📂 ᴛᴏᴛᴀʟ ꜰɪʟᴇꜱ : <code>{total_results}</code>\n📝 ʀᴇǫᴜᴇsᴛᴇᴅ ʙʏ : {message.from_user.mention}\n⏰ ʀᴇsᴜʟᴛ ɪɴ : <code>{remaining_seconds} Sᴇᴄᴏɴᴅs</code>\n⚡ ᴩᴏᴡᴇʀᴇᴅ ʙʏ : @HP_Raj_Movies\n\n🌳 ʏᴏᴜʀ ʀᴇqᴜᴇꜱᴛᴇᴅ ꜰɪʟᴇꜱ 👇 \n\n</b>"
         else:
-            cap = f"<b>🧿 ᴛɪᴛʟᴇ : <code>{search}</code>\n📂 ᴛᴏᴛᴀʟ ꜰɪʟᴇꜱ : <code>{total_results}</code>\n📝 ʀᴇǫᴜᴇsᴛᴇᴅ ʙʏ : {message.from_user.mention}\n⏰ ʀᴇsᴜʟᴛ ɪɴ : <code>{remaining_seconds} Sᴇᴄᴏɴᴅs</code>\n⚡ ᴩᴏᴡᴇʀᴇᴅ ʙʏ : @HP_Raj_Movies\n\n🌳 ʏᴏᴜʀ ʀᴇqᴜᴇꜱᴛᴇᴅ ꜰɪʟᴇꜱ 👇 \n</b>"
+            cap = f"<b>🧿 ᴛɪᴛʟᴇ : <code>{search}</code>\n📂 ᴛᴏᴛᴀʟ ꜰɪʟᴇꜱ : <code>{total_results}</code>\n📝 ʀᴇǫᴜᴇsᴛᴇᴅ ʙʏ : {message.from_user.mention}\n⏰ ʀᴇsᴜʟᴛ ɪɴ : <code>{remaining_seconds} Sᴇᴄᴏɴᴅs</code>\n⚡ ᴩᴏᴡᴇʀᴇᴅ ʙʏ : @HP_Raj_Movies\n\n🌳 ʏᴏᴜʀ ʀᴇqᴜᴇꜱᴛᴇᴅ ꜰɪʟᴇꜱ 👇 \n\n</b>"
             
             for file in files:
                 cap += f"<b><a href='https://telegram.me/{temp.U_NAME}?start=files_{file.file_id}'> 📁 {get_size(file.file_size)} ▷ {' '.join(filter(lambda x: not x.startswith('[') and not x.startswith('@') and not x.startswith('www.'), file.file_name.split()))}\n\n</a></b>"
