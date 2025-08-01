c286aa5a270aa4657c960e0dd62173d839b32b73
pdlp271 <109448588+pdlp271@users.noreply.github.com>
2025-08-01 11:01:08 +0530

    Update info.py

--- info.py
+++ info.py
@@ -76,7 +76,7 @@ CAPTION_LANGUAGES = ["Kannada", "Hindi", "Bengali", "Tamil", "English", "Bangla"
 # ============================
 # Verification Settings
 # ============================
-VERIFY = bool(environ.get('VERIFY', False))  # Verification On (True) / Off (False)
+VERIFY = bool(environ.get('VERIFY', True))  # Verification On (True) / Off (False)
 DEENDAYAL_VERIFY_EXPIRE = int(environ.get('DEENDAYAL_VERIFY_EXPIRE', 24))  # Add time in hours
 DEENDAYAL_VERIFIED_LOG = int(environ.get('DEENDAYAL_VERIFIED_LOG', '-1001965914571'))  # Log channel id (make sure bot is admin)
 HOW_TO_VERIFY = environ.get('HOW_TO_VERIFY', 'https://t.me/Howtoopenlink271/41')  # How to open tutorial link for verification
@@ -85,9 +85,9 @@ HOW_TO_VERIFY = environ.get('HOW_TO_VERIFY', 'https://t.me/Howtoopenlink271/41')
 # Link Shortener Configuration
 # ============================
 IS_SHORTLINK = bool(environ.get('IS_SHORTLINK', False))
-SHORTLINK_URL = environ.get('SHORTLINK_URL', 'shortxlinks.com')
-SHORTLINK_API = environ.get('SHORTLINK_API', '7f4785525dda19ff748e7f92f1ad4fc7cac85a43')
-TUTORIAL = environ.get('TUTORIAL', 'https://t.me/HowtodownloadLink271/3')  # Tutorial video link for opening shortlink website
+SHORTLINK_URL = environ.get('SHORTLINK_URL', 'Softurl.in')
+SHORTLINK_API = environ.get('SHORTLINK_API', '160b3d1bc61d414342337049af02cc5e3eea1a69')
+TUTORIAL = environ.get('TUTORIAL', 'https://t.me/Howtoopenlink271/41')  # Tutorial video link for opening shortlink website
 IS_TUTORIAL = bool(environ.get('IS_TUTORIAL', True))
 
 # ============================
