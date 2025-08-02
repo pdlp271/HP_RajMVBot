5da2e87877d1de6ab8c9ade5062c3cd8035c46b9
pdlp271 <109448588+pdlp271@users.noreply.github.com>
2025-08-02 11:58:04 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -331,7 +331,7 @@ async def start(client, message):
             ]]
             await message.reply_photo(
                 photo="https://graph.org/file/6928de1539e2e80e47fb8.jpg",
-                caption=f"<b>Hey {message.from_user.mention},\n\n𝑌𝑜𝑢 𝐴𝑟𝑒 𝑆𝑢𝑐𝑐𝑒𝑠𝑠𝑓𝑢𝑙 𝑉𝑒𝑟𝑖𝑓𝑖𝑒𝑑 ! 𝑁𝑜𝑤 𝑌𝑜𝑢 𝐻𝑎𝑣𝑒 𝑈𝑛𝑙𝑖𝑚𝑖𝑡𝑒𝑑 𝐴𝑐𝑐𝑒𝑠𝑠 𝐹𝑜𝑟 {DEENDAYAL_VERIFY_EXPIRE} HOURS.\n\n<blockquote>आप सफलतापूर्वक सत्यापित हो गए हैं अब आप {DEENDAYAL_VERIFY_EXPIRE}  घंटे  तक 𝐷𝑖𝑟𝑒𝑐𝑡 𝑀𝑜𝑣𝑖𝑒 की 𝐹𝑖𝑙𝑒𝑠 प्राप्त कर सकते हैं।</blockquote></b>",
+                caption=f"<b>Hey {message.from_user.mention},\n\n𝑌𝑜𝑢 𝐴𝑟𝑒 𝑆𝑢𝑐𝑐𝑒𝑠𝑠𝑓𝑢𝑙 𝑉𝑒𝑟𝑖𝑓𝑖𝑒𝑑 ! 𝑁𝑜𝑤 𝑌𝑜𝑢 𝐻𝑎𝑣𝑒 𝑈𝑛𝑙𝑖𝑚𝑖𝑡𝑒𝑑 𝐴𝑐𝑐𝑒𝑠𝑠 𝐹𝑜𝑟 {DEENDAYAL_VERIFY_EXPIRE} HOURS.\n\n<blockquote> ನಿಮ್ಮ Verification ಯಶಸ್ವಿಯಾಗಿ. ಈಗ ನೀವು {DEENDAYAL_VERIFY_EXPIRE} ಗಂಟೆಗಳ ತನಕ Unlimited Files ತಗೊಬವುದು</blockquote></b>",
                 reply_markup=InlineKeyboardMarkup(btn)
             )
             await verify_user(client, userid, token) 
@@ -445,7 +445,7 @@ async def start(client, message):
                        InlineKeyboardButton("⚡ Hᴏᴡ ᴛᴏ ᴠᴇʀɪғʏ ⚡", url=HOW_TO_VERIFY)
                    ]]
                     l = await message.reply_text(
-                        text=f"<b>♨️ YOU ARE NOT VERIFIED !\nKINDLY VERIFY TO CONTINUE & YOU CAN GET UNLIMITED ACCESS FOR {DEENDAYAL_VERIFY_EXPIRE} HOURS ✅\n\n<blockquote>⚜️ इस BOT से MOVIE प्राप्त करने के लिए आपको VERIFY करना आवश्यक है VERIFY करने के बाद आप {DEENDAYAL_VERIFY_EXPIRE} घंटे तक UNLIMITED MOVIES प्राप्त कर सकते हो।</blockquote>\n\n<blockquote>🔥 अगर आप VERIFY नहीं करना चाहते तो आप PREMIUM ले सकते हो, PREMIUM लेने के बाद आप UNLIMITED MOVIES प्राप्त कर पाओगे और आपको VERIFY करने कोई जरूरत नहीं है\n\nPLAN DETAILS के लिए CLICK करें /plan</blockquote></b>",
+                        text=f"<b>♨️ YOU ARE NOT VERIFIED !\nKINDLY VERIFY TO CONTINUE & YOU CAN GET UNLIMITED ACCESS FOR {DEENDAYAL_VERIFY_EXPIRE} HOURS ✅\n\n<blockquote>⚜️ ನೀವು Bot Use ಮಾಡಬೇಕು ಎಂದರೆ ನೀವು verify ಮಾಡಬೇಕಾಗುತ್ತದೆ. ನೀವು ಒಂದು ಬಾರಿ Verify ಆದರೆ {DEENDAYAL_VERIFY_EXPIRE} ಗಂಟೆಗಳತನಕ ಉಚಿತವಾಗಿ ಮತ್ತು Unlimited Files Download ಮಾಡಬವುದು</blockquote>\n\n<blockquote>🔥 ನೀವು Verify ಮಾಡುವುದಿಲ್ಲ ಎಂದರೆ ನೀವು ಬೊಟ್ Premium ಕೋಟವನ್ನು ಪಡೆಯಿರಿ ಆಗ Verify ಅಗತ್ಯವಿರುವುದಿಲ್ಲ \n\nPLAN DETAILS ಗಳಿಗಾಗಿ CLICK ಮಾಡಿ /plan</blockquote></b>",
                         protect_content=False,
                         reply_markup=InlineKeyboardMarkup(btn)
                     )
@@ -525,7 +525,7 @@ async def start(client, message):
                         InlineKeyboardButton("⚡ Hᴏᴡ ᴛᴏ ᴠᴇʀɪғʏ ⚡", url=HOW_TO_VERIFY)
                    ]]
                    l = await message.reply_text(
-                       text=f"<b>♨️ YOU ARE NOT VERIFIED !\nKINDLY VERIFY TO CONTINUE & YOU CAN GET UNLIMITED ACCESS FOR {DEENDAYAL_VERIFY_EXPIRE} HOURS ✅\n\n<blockquote>⚜️ इस BOT से MOVIE प्राप्त करने के लिए आपको VERIFY करना आवश्यक है VERIFY करने के बाद आप {DEENDAYAL_VERIFY_EXPIRE} घंटे  तक UNLIMITED MOVIES प्राप्त कर सकते हो। <blockquote>\n\n<blockquote>🔥 अगर आप VERIFY नहीं करना चाहते तो आप PREMIUM ले सकते हो, PREMIUM लेने के बाद आप UNLIMITED MOVIES प्राप्त कर पाओगे और आपको VERIFY करने कोई जरूरत नही है\n\nPLAN DETAILS के लिए CLICK करें /plan</blockquote></b>",
+                       text=f"<b>♨️ YOU ARE NOT VERIFIED !\nKINDLY VERIFY TO CONTINUE & YOU CAN GET UNLIMITED ACCESS FOR {DEENDAYAL_VERIFY_EXPIRE} HOURS ✅\n\n<blockquote>⚜️ ನೀವು Bot Use ಮಾಡಬೇಕು ಎಂದರೆ ನೀವು verify ಮಾಡಬೇಕಾಗುತ್ತದೆ. ನೀವು ಒಂದು ಬಾರಿ Verify ಆದರೆ {DEENDAYAL_VERIFY_EXPIRE} ಗಂಟೆಗಳತನಕ ಉಚಿತವಾಗಿ ಮತ್ತು Unlimited Files Download ಮಾಡಬವುದು<blockquote>\n\n<blockquote>🔥 ನೀವು Verify ಮಾಡುವುದಿಲ್ಲ ಎಂದರೆ ನೀವು Bot Premium ಕೋಟವನ್ನು ಪಡೆಯಿರಿ ಆಗ Verify ಅಗತ್ಯವಿರುವುದಿಲ್ಲ\n\nPLAN DETAILS ಗಳಿಗಾಗಿ CLICK ಮಾಡಿ /plan</blockquote></b>",
                        protect_content=False,
                        reply_markup=InlineKeyboardMarkup(btn)
                    )
