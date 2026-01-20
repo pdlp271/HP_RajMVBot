36c98aefbe9885b66e15bf60e34f1ae5d9c56f7e
pdlp271 <109448588+pdlp271@users.noreply.github.com>
2026-01-20 23:37:49 +0530

    Update tutorial link in info.py

--- info.py
+++ info.py
@@ -87,7 +87,7 @@ HOW_TO_VERIFY = environ.get('HOW_TO_VERIFY', 'https://t.me/HowtoopenLink02/6')  
 IS_SHORTLINK = bool(environ.get('IS_SHORTLINK', False))
 SHORTLINK_URL = environ.get('SHORTLINK_URL', 'softurl.in')
 SHORTLINK_API = environ.get('SHORTLINK_API', '160b3d1bc61d414342337049af02cc5e3eea1a69')
-TUTORIAL = environ.get('TUTORIAL', 'https://t.me/HowtoopenLink02/6')  # Tutorial video link for opening shortlink website
+TUTORIAL = environ.get('TUTORIAL', 'https://t.me/HowtoopenLink02/7')  # Tutorial video link for opening shortlink website
 IS_TUTORIAL = bool(environ.get('IS_TUTORIAL', True))
 
 # ============================
